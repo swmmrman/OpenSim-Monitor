009aeb63f42b4492571bb87b326ccc2e843aa626
Stephan Kern <stephan@swmmrman.com>
2022-03-10 22:42:21 -0700

    Needed imports

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import psutil
+from time import sleep
+
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('user')
 
