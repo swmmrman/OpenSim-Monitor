1f1bdc6a766090123e501958ffa8b57123cc69f7
Stephan Kern <stephan@swmmrman.com>
2022-03-11 00:52:54 -0700

    Formatting and remove hi function.

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from time import sleep
 
 sim_list = []
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def get_sim_list(filename='simlist.txt'):
     try:
@@ -14,8 +12,9 @@ def get_sim_list(filename='simlist.txt'):
         print(F"The sim list file \"{filename}\" was not found.")
         sys.exit(1)
     return the_list
+
+
 if __name__ == '__main__':
-    print_hi('user')
     sim_list = get_sim_list()
     print(sim_list)
 
